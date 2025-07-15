27b72a320c0a388ff3e1811ec8f05a0fd334db1e
silentinstalls <silentinstalls@gmail.com>
2025-07-15 20:21:07 +0700

    Create 7zip.ps1

---
+++ 7zip.ps1
@@ -0,0 +1,32 @@
+
+# GitHub API URL for the app manifest.
+$apiUrl = "https://api.github.com/repos/microsoft/winget-pkgs/contents/manifests/7/7zip/7zip"
+
+# Fetch version folders then filter only version folders.
+$versions = Invoke-RestMethod -Uri $apiUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+$versionFolders = $versions | Where-Object { $_.type -eq "dir" }
+
+# Extract and sort version numbers to get the latest version.
+$sortedVersions = $versionFolders | ForEach-Object { $_.name } | Sort-Object {[version]$_} -Descending -ErrorAction SilentlyContinue
+$latestVersion = $sortedVersions[0]
+
+# Get contents of the latest version folder to find the .installer.yaml file.
+$latestApiUrl = "$apiUrl/$latestVersion"
+$latestFiles = Invoke-RestMethod -Uri $latestApiUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+$installerFile = $latestFiles | Where-Object { $_.name -like "*.installer.yaml" }
+
+# Download and parse YAML content to get the Url of the latest installer file.
+$yamlUrl = $installerFile.download_url
+$yamlContent = Invoke-RestMethod -Uri $yamlUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+$null = ($yamlContent -join "`n") -match "InstallerUrl:\s+(http.*)"
+$installerUrl = $Matches[1]
+
+# Download the latest installer to the temp folder.
+$webClient = [System.Net.WebClient]::new()
+$webClient.DownloadFile($installerUrl, "$env:TEMP\7zip-latest.exe")
+
+# Start the install process.
+Start-Process -FilePath "$env:TEMP\7zip-latest.exe" -ArgumentList '/S' -Wait
+
+# Delete the downloaded installer file.
+Remove-Item -Path "$env:TEMP\7zip-latest.exe" -Force -ErrorAction SilentlyContinue
