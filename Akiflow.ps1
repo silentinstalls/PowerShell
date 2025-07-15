e810f4c5cbe8bf76c3f873dbd1bfc67e9c9531b8
silentinstalls <silentinstalls@gmail.com>
2025-07-15 22:05:10 +0700

    Update Akiflow.ps1

--- Akiflow.ps1
+++ Akiflow.ps1
@@ -1,1 +1,31 @@
+# GitHub API URL for the app manifest.
+$apiUrl = "https://api.github.com/repos/microsoft/winget-pkgs/contents/manifests/a/Akiflow/Akiflow"
 
+# Fetch version folders then filter only version folders.
+$versions = Invoke-RestMethod -Uri $apiUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+$versionFolders = $versions | Where-Object { $_.type -eq "dir" }
+
+# Extract and sort version numbers to get the latest version.
+$sortedVersions = $versionFolders | ForEach-Object { $_.name } | Sort-Object {[version]$_} -Descending -ErrorAction SilentlyContinue
+$latestVersion = $sortedVersions[0]
+
+# Get contents of the latest version folder to find the .installer.yaml file.
+$latestApiUrl = "$apiUrl/$latestVersion"
+$latestFiles = Invoke-RestMethod -Uri $latestApiUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+$installerFile = $latestFiles | Where-Object { $_.name -like "*.installer.yaml" }
+
+# Download and parse YAML content to get the Url of the latest installer file.
+$yamlUrl = $installerFile.download_url
+$yamlContent = Invoke-RestMethod -Uri $yamlUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+$yamlString = $yamlContent -join "`n"
+$installerUrl = [regex]::Matches($yamlString, "InstallerUrl:\s+(http[^\s]+)") | ForEach-Object { $_.Groups[1].Value }
+
+# Download the latest installer to the temp folder.
+$webClient = [System.Net.WebClient]::new()
+$webClient.DownloadFile($installerUrl, "$env:TEMP\Akiflow-latest.exe")
+
+# Start the install process.
+Start-Process -FilePath "$env:TEMP\Akiflow-latest.exe" -ArgumentList '/S /NCRC /AllUsers' -Wait
+
+# Delete the downloaded installer file.
+Remove-Item -Path "$env:TEMP\Akiflow-latest.exe" -Force -ErrorAction SilentlyContinue
