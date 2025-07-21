9c55d343dc9a46c00947e9bb784d7cfefb4ef63d
silentinstalls <silentinstalls@gmail.com>
2025-07-21 20:59:37 +0700

    Create MicrosoftTeams.ps1

---
+++ MicrosoftTeams.ps1
@@ -0,0 +1,41 @@
+# Define the GitHub API URL for the app manifests in winget-pkgs.
+$apiUrl = "https://api.github.com/repos/microsoft/winget-pkgs/contents/manifests/m/Microsoft/Teams"
+
+# Fetch version folders then filter only version folders.
+$versions = Invoke-RestMethod -Uri $apiUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+$versionFolders = $versions | Where-Object { $_.type -eq "dir" }
+
+# Extract and sort version numbers to get the latest version.
+$sortedVersions = $versionFolders | ForEach-Object { $_.name } | Sort-Object {[version]$_} -Descending -ErrorAction SilentlyContinue
+$latestVersion = $sortedVersions[0]
+
+Write-Host "Latest Microsoft Teams version: $latestVersion."
+
+# Get contents of the latest version folder to find the .installer.yaml file.
+$latestApiUrl = "$apiUrl/$latestVersion"
+$latestFiles = Invoke-RestMethod -Uri $latestApiUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+$installerFile = $latestFiles | Where-Object { $_.name -like "*.installer.yaml" }
+
+# Download and parse YAML content to get the Url of the latest installer file.
+$yamlUrl = $installerFile.download_url
+$yamlContent = Invoke-RestMethod -Uri $yamlUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+$yamlString = $yamlContent -join "`n"
+$installerUrls = [regex]::Matches($yamlString, "InstallerUrl:\s+(http[^\s]+)") | ForEach-Object { $_.Groups[1].Value }
+$installerUrl = $installerUrls[1]
+
+Write-Host "Downloading installer from: $installerUrl"
+
+# Download the latest installer to the temp folder.
+$webClient = [System.Net.WebClient]::new()
+$webClient.DownloadFile($installerUrl, "$env:TEMP\MSTeams.msix")
+$teambootstrapperUrl = 'https://go.microsoft.com/fwlink/?linkid=2243204&clcid=0x409'
+$webClient.DownloadFile($teambootstrapperUrl, "$env:TEMP\teamsbootstrapper.exe")
+
+# Start the install process.
+& "$env:TEMP\teamsbootstrapper.exe" -p -o "$env:TEMP\MSTeams.msix"
+
+# Delete the downloaded installer file.
+Remove-Item -Path "$env:TEMP\MSTeams.msix" -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "$env:TEMP\teamsbootstrapper.exe" -Force -ErrorAction SilentlyContinue
+
+Write-Host "Microsoft Teams installation completed."
