b88bc6d988f043dda462d4bcbbb68e0a3d26f8d1
silentinstalls <silentinstalls@gmail.com>
2025-07-23 09:46:12 +0700

    Create VNCServer.ps1

---
+++ VNCServer.ps1
@@ -0,0 +1,42 @@
+# Define the GitHub API URL for the app manifests in winget-pkgs.
+$apiUrl = "https://api.github.com/repos/microsoft/winget-pkgs/contents/manifests/r/RealVNC/VNCServer"
+
+# Fetch version folders then filter only version folders.
+$header         = @{ "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" }
+$versions       = Invoke-RestMethod -Uri $apiUrl -Headers $header
+$versionFolders = $versions | Where-Object { $_.type -eq "dir" }
+
+# Extract and sort version numbers to get the latest version.
+$sortedVersions = $versionFolders | ForEach-Object { $_.name } | Sort-Object {[version]$_} -Descending -ErrorAction SilentlyContinue
+$latestVersion  = $sortedVersions[0]
+
+Write-Host "Latest VNCServer version: $latestVersion."
+
+# Get contents of the latest version folder to find the .installer.yaml file.
+$latestApiUrl  = "$apiUrl/$latestVersion"
+$latestFiles   = Invoke-RestMethod -Uri $latestApiUrl -Headers $header
+$installerFile = $latestFiles | Where-Object { $_.Name -like "*.installer.yaml" }
+
+# Download and parse YAML content to get the Url of the latest installer file.
+$yamlUrl        = $installerFile.download_url
+$yamlContent    = Invoke-RestMethod -Uri $yamlUrl -Headers $header
+$yamlString     = $yamlContent -join "`n"
+$installerUrls  = [regex]::Matches($yamlString, "InstallerUrl:\s+(http[^\s]+)") | ForEach-Object { $_.Groups[1].Value }
+$installerUrl   = $installerUrls[1]
+
+Write-Host "Downloading installer from: $installerUrl"
+
+# Download the latest installer to the temp folder.
+$webClient = [System.Net.WebClient]::new()
+$webClient.DownloadFile($installerUrl, "$env:TEMP\VNCServer-latest.zip")
+Expand-Archive -Path "$env:TEMP\VNCServer-latest.zip" -DestinationPath "$env:TEMP\VNCServer" -Force
+
+# Start the install process.
+Set-Location -Path $env:TEMP\VNCServer
+msiexec.exe /i $((Get-ChildItem -Path $env:TEMP\VNCServer -Filter *64bit*).Name) /qn
+
+# Delete the downloaded installer file.
+Remove-Item -Path "$env:TEMP\VNCServer-latest.zip" -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "$env:TEMP\VNCServer" -Recurse  -Force -ErrorAction SilentlyContinue
+
+Write-Host "VNCServer installation completed."
